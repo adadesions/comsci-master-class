58b580d2958e95a70d839d9b191435c870b73a84
adadesions <adadesions@gmail.com>
2020-10-18 14:43:23 +0700

    Imposters

--- amongUs.js
+++ amongUs.js
@@ -1,6 +1,6 @@
 // Crewmate vs Immposter
-const numPlayer = 4
-const numImposter = 1
+const numPlayer = 10
+const numImposter = 5
 
 function initPlayerState(numPlayer) {
     let playerState = []
@@ -11,14 +11,25 @@ function initPlayerState(numPlayer) {
     return playerState
 }
 
+function createImposter(playerState, numImposter) {
+    let imposterIndexes = []
+    while (imposterIndexes.length < numImposter) {
+        let idx = Math.floor(Math.random() * 1000) % numPlayer
+        if (imposterIndexes.indexOf(idx) < 0) {
+            imposterIndexes.push(idx)
+            playerState[idx] = 1
+        }
+    }
+
+    return playerState
+}
+
 function main() {
     // 0: crewmate, 1: imposter
     let playerState = initPlayerState(numPlayer)
 
     console.log("Before random:", playerState)
-    const imposterIndex = Math.floor(Math.random() * 10) % numPlayer
-    playerState[imposterIndex] = 1
-
+    playerState = createImposter(playerState, numImposter)
     console.log("After random:", playerState)
 }
 
